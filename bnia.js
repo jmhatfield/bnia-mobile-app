335656281ba4e9b0bce27b86224efcd943433dcd
Jessica Hatfield <jhatfiel@terpmail.umd.edu>
2019-10-28 21:53:39 -0400

    adds basic loading and onboarding pages

--- bnia.js
+++ bnia.js
@@ -1,1 +1,29 @@
 // all of our JS goes here
+$(document).ready(function() {
+    // Inital load
+    showLoadingPage();
+
+    // Change to onboarding
+    setTimeout(function() {
+        showOnboardingPage();
+    }, 2000)
+
+});
+
+function showLoadingPage() {
+    $("#loading").removeClass("hide");
+    $("#onboarding").addClass("hide");
+    $("#content").addClass("hide");
+}
+
+function showOnboardingPage() {
+    $("#loading").addClass("hide");
+    $("#onboarding").removeClass("hide");
+    $("#content").addClass("hide");
+}
+
+function showContentPage() {
+    $("#loading").addClass("hide");
+    $("#onboarding").addClass("hide");
+    $("#content").removeClass("hide");
+}

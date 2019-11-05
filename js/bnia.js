e823c5281011aec10da969d7cb2854775e7818bf
Jessica Hatfield <jhatfiel@terpmail.umd.edu>
2019-11-04 22:20:26 -0500

    made more changes

--- js/bnia.js
+++ js/bnia.js
@@ -11,8 +11,10 @@ $(document).ready(function() {
     addIndicatorOptions();
     addNeighborhoodOptions();
 
-    setupMap();
-    setupTable();
+    let fakeData = generateFakeData();
+
+    setupMap(fakeData);
+    setupTable(fakeData);
 });
 
 function showLoadingPage() {
@@ -131,20 +133,58 @@ function showListContent() {
     $("#listContent").removeClass("hide");
 }
 
-function setupMap() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+function generateFakeData() {
+    let data = {};
+    neighborhoods.forEach(function(ele) {
+        data[ele] = Math.floor(Math.random() * 100);
+    });
+    return data;
+}
+
+function getColor(d) {
+	return d > 1000 ? '#800026' :
+	       d > 500  ? '#BD0026' :
+	       d > 200  ? '#E31A1C' :
+	       d > 100  ? '#FC4E2A' :
+	       d > 50   ? '#FD8D3C' :
+	       d > 20   ? '#FEB24C' :
+	       d > 10   ? '#FED976' :
+	                  '#FFEDA0';
+}
+
+function setupMap(fakeData) {
+    var mymap = L.map('mapid').setView([39.290, -76.612], 12);
     mymap.removeControl(mymap.zoomControl);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     	maxZoom: 18,
     	id: 'mapbox.streets',
     	accessToken: 'pk.eyJ1Ijoiam1oYXRmaWVsZCIsImEiOiJjazJnbjlpbjUwMHg1M2JxZWR2aHQ2cjkyIn0.hI5VG7I0cGyKhFN_yBUhiQ'
     }).addTo(mymap);
+
+    // var legend = L.control({position: 'bottomright'});
+    // legend.onAdd = function (mymap) {
+    //
+    // 	var div = L.DomUtil.create('div', 'info legend'),
+    // 		grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+    // 		labels = [];
+    //
+    // 	// loop through our density intervals and generate a label with a colored square for each interval
+    // 	for (var i = 0; i < grades.length; i++) {
+    // 		div.innerHTML +=
+    // 			'<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+    // 			grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    // 	}
+    //
+    // 	return div;
+    // };
+    //
+    // legend.addTo(mymap);
 }
 
-function setupTable() {
+function setupTable(fakeData) {
     let html = "";
     neighborhoods.forEach(function(ele) {
-        html += "<tr><td>" + ele + "</td><td>temp</td></tr>";
+        html += "<tr><td>" + ele + "</td><td class='text-right'>" + fakeData[ele] + "</td></tr>";
     });
     $("#tableBody").html(html);
 }
